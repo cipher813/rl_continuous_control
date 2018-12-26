def109429689f1c07dc88cdf30ee16bbd014e416
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2018-12-26 08:11:15 -0800

    updated with google cloud path info

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,9 @@ from agents.DDPG import DDPG
 from agents.TD3 import TD3
 from util import *
 
-PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_continuous_control/"
+# PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_continuous_control/"
 # PATH = "/home/cipher813/rl_continuous_control/"
+PATH = "/home/bcm822_gmail_com/rl_continuous_control/"
 RESULT_PATH = PATH + "results/"
 
 agent_dict = {
@@ -12,4 +13,5 @@ agent_dict = {
              }
 
 # scores = train_gym(PATH, "BipedalWalker-v2",agent_dict)
-scores = train_unity(PATH, "Reacher20.app",agent_dict)
+# scores = train_unity(PATH, "Reacher20.app",agent_dict)
+scores = train_unity(PATH,"Reacher_Linux_NoVis/Reacher.x86_64",agent_dict)
