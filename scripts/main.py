23bff0fda6201b1894992729c52236d1b711e1a8
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2018-12-27 10:38:56 -0800

    successful train of DDPG with basic algo

--- scripts/main.py
+++ scripts/main.py
@@ -2,9 +2,9 @@ from agents.DDPG import DDPG
 from agents.TD3 import TD3
 from util import *
 
-# PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_continuous_control/"
+PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_continuous_control/"
 # PATH = "/home/cipher813/rl_continuous_control/"
-PATH = "/home/bcm822_gmail_com/rl_continuous_control/"
+# PATH = "/home/bcm822_gmail_com/rl_continuous_control/"
 RESULT_PATH = PATH + "results/"
 
 agent_dict = {
@@ -13,5 +13,5 @@ agent_dict = {
              }
 
 # scores = train_gym(PATH, "BipedalWalker-v2",agent_dict)
-# scores = train_unity(PATH, "Reacher20","Reacher20.app",agent_dict)
-scores = train_unity(PATH,"Reacher20","Reacher_Linux_NoVis2/Reacher.x86_64",agent_dict)
+scores = train_unity(PATH, "Reacher20","Reacher20.app",agent_dict)
+# scores = train_unity(PATH,"Reacher20","Reacher_Linux_NoVis2/Reacher.x86_64",agent_dict)
