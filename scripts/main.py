4867272df79bbc72703e2c9f25f211ab905707ab
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2019-01-01 17:12:29 -0800

    configure for azure

--- scripts/main.py
+++ scripts/main.py
@@ -5,12 +5,12 @@ This may be fixed in mlagents.envs but have not verified.
 """
 from agents.DDPG import DDPG
 from agents.D4PG import D4PG
-from agents.DDPGplus import DDPGplus
+# from agents.DDPGplus import DDPGplus
 from agents.TD3 import TD3
 from util import *
 
-PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_continuous_control/"
-# PATH = "/home/cipher813/rl_continuous_control/"
+# PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_continuous_control/"
+PATH = "/home/cipher813/rl_continuous_control/"
 # PATH = "/home/bcm822_gmail_com/rl_continuous_control/"
 RESULT_PATH = PATH + "results/"
 
@@ -19,10 +19,10 @@ RESULT_PATH = PATH + "results/"
 TRAIN_MODE = "multi"
 
 env_dict = {
-            "Reacher20":["unity","Reacher20.app","multi",30.0], # 30.0
-            # "Reacher20":["unity","Reacher_Linux_NoVis2/Reacher.x86_64","multi",30.0],
-            "Reacher1":["unity","Reacher1.app","single",30.0], # 30.0
-            # "Reacher1":["unity","Reacher_Linux_NoVis1/Reacher.x86_64","single",30.0],
+            # "Reacher20":["unity","Reacher20.app","multi",30.0], # 30.0
+            "Reacher20":["unity","Reacher_Linux_NoVis2/Reacher.x86_64","multi",30.0],
+            # "Reacher1":["unity","Reacher1.app","single",30.0], # 30.0
+            "Reacher1":["unity","Reacher_Linux_NoVis1/Reacher.x86_64","single",30.0],
             "Pendulum":["gym","Pendulum-v0","single",2000.0],
             "BipedalWalker":["gym","BipedalWalker-v2","single",300.0] # 300.0
             }
@@ -30,8 +30,8 @@ env_dict = {
 # BUG cannot run more than one agent in a single unity environment in same loop
 # per open issue at https://github.com/Unity-Technologies/ml-agents/issues/1167
 agent_dict = {
-              # "DDPG":[DDPG,"both"],
-              "DDPGplus":[DDPGplus,"both"],
+              "DDPG":[DDPG,"both"],
+              # "DDPGplus":[DDPGplus,"both"],
               # "D4PG":[D4PG,"single"],
               # "TD3":[TD3,"both"]
              }
